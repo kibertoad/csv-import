caeebedc9600e8ae44fcf40551cae16caf8a864e
Igor Savin <iselwin@gmail.com>
2021-12-17 00:49:16 +0200

    Export more types

--- index.ts
+++ index.ts
@@ -7,6 +7,9 @@ export {
   toOptionalDate,
   toMandatoryDate,
   toOptionalBoolean,
+  ErrorCodes,
 } from './lib/mappers'
 
 export type { ColumnDefinition, ColumnDefinitions, ImportResult } from './lib/csvImporter'
+export type { Mapper } from './lib/mappers'
+export type { ValidationContext } from './lib/validation'
